23eadf5a9a107001029926569441881b347cd85c
brahimsouidene1994 <72886850+brahimsouidene1994@users.noreply.github.com>
2021-07-15 17:53:58 +0100

    fix ui

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const tl = gsap.timeline({defaults:{ease:"power1.out"}});
 
 tl.to('.text-hide',{y:'0%', duration:1, stagger:0.25});
 tl.to('.slider-intro',{y:'-100%', duration:1.5, delay:0.5});
-tl.to('.intro',{y:'-100%', duration:1},"-=1");
+tl.to('.intro',{y:'-100%', duration:1},"-=1.25");
 tl.fromTo('.starting-text',{opacity:0},{opacity:1, duration:1});
 
 /**intro animation */
@@ -29,47 +29,44 @@ const links = document.querySelectorAll(".navbar-container li");
 const img_container = document.querySelector(".profil-img");
 const img = document.querySelector(".profil-img > img");
 
-window.onscroll = ()=>{
-    // pageYOffset or scrollY
-    let y = window.pageYOffset ;
-  if (y > 0) {
-    navbar.classList.add('scrolled')
-  } else {
-    navbar.classList.remove('scrolled')
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+    if(currentScrollPos === 0){
+        document.getElementById("navbar").style.background = "none";
+    }else{
+        document.getElementById("navbar").style.background = "rgb(40, 54, 24)";
+    }
   }
+  else {
+    document.getElementById("navbar").style.top = "-10%";  
 }
+  prevScrollpos = currentScrollPos;
+}
+// }
+
 
 hamburger.addEventListener('click',()=>{
     nav_links.classList.toggle('open');   
-});
-links.forEach(link => {
-    link.classList.toggle('fade');
-    link.addEventListener('click',()=>{
-        if(nav_links.classList.toggle("open")){
-            nav_links.classList.toggle("open")
-            links.forEach(item=>{
-                if(item.classList.toggle("fade")){
-                    item.classList.toggle("fade")
-                }
-            })
-        }
+    links.forEach(link => {
+        link.classList.toggle('fade');
+        link.addEventListener('click',()=>{
+            if(nav_links.classList.toggle("open")){
+                nav_links.classList.toggle("open")
+                links.forEach(item=>{
+                    if(item.classList.toggle("fade")){
+                        item.classList.toggle("fade")
+                    }
+                })
+            }
+        });
     });
 });
-img_container.addEventListener('click',()=>{
-    img_container.classList.toggle('open');
-    img.classList.toggle('open');
-})
-/** menu navbar mobile*/
-
-var controller = new ScrollMagic.Controller();
-
-var scene1 = new ScrollMagic.Scene({
-    triggerElement : '.aboutme-container'
-})
-.setClassToggle('.profil-img','show')
-.addTo(controller);
 
 /**academy animation */
+var controller = new ScrollMagic.Controller();
 var sceneAcademy1 = new ScrollMagic.Scene({
     triggerElement : '.historique-container .historique-body'
 })
@@ -119,3 +116,18 @@ img_profil_container.addEventListener('mousemove',moveSlider);
 /**image profil */
 
 /**skills animation */
+
+
+$(document).ready(function(){
+    $("#btn-footer").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#footer-part").offset().top
+        }, 2000);
+    });
+  });
+
+function  goToContact(){
+    $('html, body').animate({
+        scrollTop: $("#footer-part").offset().top
+    }, 2000);
+}
